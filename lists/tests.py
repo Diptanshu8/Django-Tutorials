f565600e8ae2f86b28fb5bbd17c9157e93c277d2
vagrant <vagrant@precise64.(none)>
2016-05-14 19:37:22 +0000

    This is an important commit in this a base url has been triggered and linked to home_page in lists/views

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 # Create your tests here.
-class tempclass(TestCase):
-	def test_first(self):
-		self.assertEqual(1+1,3)
+class tempclass_homepagetest(TestCase):
+	def test_to_test_homepage(self):
+		found = resolve('/')
+		self.assertEqual(found.func,home_page)
