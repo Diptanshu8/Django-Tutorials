e86ca7820496eedff34a4164a61e6f649de2770b
vagrant <vagrant@precise64.(none)>
2016-05-30 17:50:28 +0000

    Basic HTTPResponse for homepage has been set to check whether To-Do is present or not

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequests 
 
 # Create your tests here.
 class tempclass_homepagetest(TestCase):
 	def test_to_test_homepage(self):
 		found = resolve('/')
 		self.assertEqual(found.func,home_page)
+	def test_to_httpresponse(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertIn(b'<title>To-Do</title>',response.content)	
