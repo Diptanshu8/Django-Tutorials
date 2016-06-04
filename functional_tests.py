123b2f596e8c1dac2617ffcd7b995b7e8008be15
vagrant <vagrant@precise64.(none)>
2016-06-04 08:02:53 +0000

    Functional test file oommit functional test to add entry to the dialog box of TO-Do list and verfication of the addition has been done

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,30 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 class testclass(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+#		self.browser.implicitly_wait(3)
 	def tearDown(self):
 		self.browser.quit()
 	def test_first(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do',self.browser.title)
+		
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
+
+		input_box = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(input_box.get_attribute('placeholder'),'Enter to-do item')
+		
+		input_box.send_keys("Learn Django")
+		input_box.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1:Learn Django' for row in rows))
+		
 		self.fail("Finish the test !!")
 if __name__ == '__main__':
 	unittest.main()
